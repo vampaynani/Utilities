1b9cbf462531c6423e8f15aa8dd5e5a968965f4d
Wenceslao Negrete <vampediem@gmail.com>
2012-12-13 13:45:19 -0600

    twitter 4 bloggers

--- twitter4bloggers.js
+++ twitter4bloggers.js
@@ -1,12 +1,12 @@
 //===== General Vars =======
 var username = 'username';
-var twitnumber = '1';
+var twitnumber = '0';
 //======================
 
-$(document).ready(function(){
-	$.get('https://api.twitter.com/1/statuses/user_timeline.json', {'screen_name':username,'count':twitnumber}, function(data){
+$(document).on('ready', function(){
+	$.get('https://api.twitter.com/1/statuses/user_timeline.json', {'screen_name':username, 'count':twitnumber}, function(data){
 		$.each(data, function(index, item) {
-			$('#twitter-list').append(item.screen_name+": "+item.text);
+			$('#twitter-list').append(item.screen_name + ": " + item.text);
 		})
 	}, "jsonp");
 });
