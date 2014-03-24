97b2fdf088e654ee33b44464a672706a8b68fbff
Wenceslao Negrete <vampediem@gmail.com>
2014-03-24 01:17:13 -0600

    config.php correction

--- PDO/src/config.php
+++ PDO/src/config.php
@@ -3,7 +3,7 @@
 define('DB_TYPE', 'mysql');
 define('DB_SRVR', 'localhost');
 define('DB_USR', 'user');
-define('DB_PWD', 'Z(password');
+define('DB_PWD', 'password');
 define('DB_NAME', 'dbname');
 */
 define('DB_TYPE', 'sqlite');
